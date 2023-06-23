4581af4bc72ca17b3752d1ff06d8526404a207fd
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-06-23 21:46:16 +0530

    1

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -2,7 +2,13 @@ import React from 'react'
 import UsersList from '../components/UsersList'
 
 const Users = () => {
-  return <UsersList/>;
+  const USERS =[{
+    id: 'u1',
+    name: 'Max Schwarz',
+    image: 'https://images.unsplash.com/photo-1612833603929-4b6b7e2b2b0f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',
+    places: 3
+  }]
+  return <UsersList items={USERS}/>
 }
 
 export default Users
