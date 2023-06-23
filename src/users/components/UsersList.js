ed2a7ec8c649f00485b9b5a41cee0657d4346290
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-06-23 21:44:15 +0530

    1

--- src/users/components/UsersList.js
+++ src/users/components/UsersList.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import UsersItem from './UsersItem'
 import "../components/UsersList.css"
 
-const UsersList = () => {
-  return (
-    <div>UsersList</div>
-  )
+const UsersList = props => {
+    if (props.items.length === 0) {
+        return (
+            <div>No Users found !</div>
+        )
+    }
+
+    return (
+        <ul>
+            {props.items.map(users => (
+                <UsersItem 
+                    key={users.id} 
+                    id={users.id} 
+                    image={users.image} 
+                    name={users.name} 
+                    placeCount={users.place} />
+            ))};
+
+        </ul>
+    )
 }
 
 export default UsersList
--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,9 +1,8 @@
 import React from 'react'
+import UsersList from '../components/UsersList'
 
 const Users = () => {
-  return (
-    <div>Users</div>
-  )
+  return <UsersList/>;
 }
 
 export default Users
