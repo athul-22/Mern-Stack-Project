fc497f56940565dd81cda4f9b5cb08dc221bc744
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-06-23 21:16:48 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router , Route , Redirect, Switch} from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
+import Users from './users/pages/Users';
+import NewPlaces from './places/pages/NewPlaces';
 
-function App() {
+const App = () =>  {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Switch>
+        <Route path="/" exact> <Users/> </Route>
+        <Route path="/newplace" exact> <NewPlaces/> </Route>
+        <Redirect to="/"/>
+        </Switch>
+      </Router>
   );
 }
 
