f2f663957c06f9d0bea694a7fc00da57b3b255fd
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-06-23 22:17:59 +0530

    1

--- src/users/components/UsersItem.js
+++ src/users/components/UsersItem.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import "../components/UsersItem.css"
+import Avatar from '../../shared/components/UIElements/Avatar/Avatar'
+import Card from '../../shared/components/UIElements/Cards/Card'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 
 const UsersItem = props => {
   return (
 
       <li className='user-item'>
         <div className='user-item__content'>
+          <Card>
+          <Link to={`/${props.id}/place`}>
           <div className='user-item__image'>
-            <img src={props.image} alt={props.name} />
+            <Avatar image={props.image} alt={props.name}/>
+            {/* <img src={props.image} alt={props.name} /> */}
           </div>
           <div className='user-item__info'>
             <h2>{props.name}</h2>
             <h3>{props.placeCount}</h3> {props.placeCount ===1 ? 'Place' : 'Places'}
           </div>
+          </Link>
+          </Card>
         </div>
       </li>
      
