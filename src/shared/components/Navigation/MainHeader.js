4ceed24215f507d33b889e0bcc5cb6ebac06c162
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-06-24 20:55:44 +0530

    1

---
+++ src/shared/components/Navigation/MainHeader.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../Navigation/MainHeader.css"
+
+const MainHeader = props => {
+    <header className="main-header">{props.children}</header>
+}
+
+export default MainHeader;
