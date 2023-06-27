d26236dc0a46201afcf879b0ac112cea8e8ca19d
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-06-27 22:16:50 +0530

    1

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -15,7 +15,7 @@ const PlaceList = props => {
 
   return(
     <div className='place-list'>
-        {props.item.map( place=> <Placeitem key={place.id} id={place.id} image={place.imageURL} title={place.title} description={place.description} address={place.address} /> )}
+        {props.item.map( place=> <Placeitem key={place.id} id={place.id} image={place.imageURL} title={place.title} description={place.description} address={place.address} creatorID={place.creator} coordinates={place.location}  /> )}
     </div>
   )
 }
--- src/places/components/Placeitem.js
+++ src/places/components/Placeitem.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import './PlaceItem.css'
+import Card from '../../shared/components/UIElements/Cards/Card'
 
-const Placeitem = () => {
+const Placeitem = props => {
   return (
-    <div>Placeitem</div>
+    <li className='place-item'>
+        <div className='place-item__image'>
+            <img src={props.image}/>
+        </div>
+        <div className='place-item__info'>
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+        </div>
+        <div className='place-item__actions'>
+            <button>View on Map</button>
+            <button>Edit</button>
+            <button>Delete</button>
+        </div>
+    </li>
   )
 }
 
