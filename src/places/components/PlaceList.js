d12351fa547b7b07cd2d17ba9588bd9ca48bf1a6
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-06-27 21:30:12 +0530

    1

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import './PlaceItem.css'
 import Card from '../../shared/components/UIElements/Cards/Card'
+import Placeitem from './Placeitem'
 
 const PlaceList = props => {
   if(props.item.length === 0){
-    return <div className='place-list center'>No places found</div>
-  }
-  return(
-    <div>
+    return <div className='place-list center'>
         <Card>
-            
+        No places found
+        <button>Share Place</button>
         </Card>
+    </div>
+  }
+
+  return(
+    <div className='place-list'>
+        {props.item.map( place=> <Placeitem key={place.id} id={place.id} image={place.imageURL} title={place.title} description={place.description} address={place.address} /> )}
     </div>
   )
 }
