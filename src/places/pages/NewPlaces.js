17119dab81bd284441d7742ccf9b07c386db5009
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-07-03 23:33:16 +0530

    1

--- src/places/pages/NewPlaces.js
+++ src/places/pages/NewPlaces.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { VALIDATOR_REQUIRE } from '../../shared/util/validator';
 import './NewPlaces.css';
 import Input from '../../shared/FormElements/input';
 
 const NewPlaces = () => {
 
-  const titleInputHandler = (id, value , isValid) => {
+  const titleInputHandler = useCallback((id, value, isValid) => {
 
-  }
+  }, []);
+
+  const descriptionHandler = useCallback((id, value, isValid) => {
+
+  }, []);
+
 
   return (
     <form className='place-form'>
-      <Input 
-      element="input" 
-      type="text" label="title" 
-      errorText="enter a valid text" 
-      validators={VALIDATOR_REQUIRE()}
-      onInput={titleInputHandler}
+      <Input
+        id="title"
+        element="input"
+        type="text" label="title"
+        errorText="enter a valid text"
+        validators={[VALIDATOR_REQUIRE()]}
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textArea"
+        type="text"
+        label="title"
+        errorText="enter a valid text"
+        validators={[VALIDATOR_REQUIRE()]}
+        onInput={descriptionHandler}
       />
     </form>
   );
