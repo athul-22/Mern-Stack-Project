577ebfb77db3e73dda48857857e52a8d2c475330
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-07-13 12:50:26 +0530

    1

--- src/places/pages/updatePlace.js
+++ src/places/pages/updatePlace.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
-const updatePlace = () => {
+const UpdatePlace = () => {
    const placeId = useParams().placeId;
     return <h1>update place</h1>
 }
 
-export default updatePlace;
+export default UpdatePlace;
