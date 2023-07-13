760c1bb8fb058294438d215274d7fb7f749e63a6
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-07-13 13:21:44 +0530

    1

--- src/places/pages/updatePlace.js
+++ src/places/pages/updatePlace.js
@@ -1,9 +1,51 @@
 import React from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
+const DUMMY_PLACES = [
+    {
+      id: 'p1',
+      title: 'Empire State Building',
+      description: 'One of the most famous sky scrapers in the world!',
+      imageUrl: 'https://www.planetware.com/wpimages/2019/11/india-best-places-to-visit-agra.jpg',
+      address: "test adddress",
+      location: {
+            lat:3003333,
+            lng:72.333,
+      },
+    },
+    {
+      id: 'p2',
+      title: 'Empire State Building',
+      description: 'One of the most famous sky scrapers in the world!',
+      imageUrl: 'https://www.planetware.com/wpimages/2019/11/india-best-places-to-visit-agra.jpg',
+      address: "test adddress",
+      location: {
+            lat:3003333,
+            lng:72.333,
+      },
+    },
+    {
+      id: 'p3',
+      title: 'pink tree',
+      description: 'One of the most famous sky scrapers in the world!',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a2/1121098-pink-nature-wallpaper-1920x1080-lockscreen.jpg',
+      address: "test adddress",
+      location: {
+            lat:3003333,
+            lng:72.333,
+      },
+    },
+  ]
+
+
 const UpdatePlace = () => {
    const placeId = useParams().placeId;
+   const identifiedPlace = DUMMY_PLACES.find(p=>p.id === placeId);
     return <h1>update place</h1>
+}
+
+if(!placeId){
+    return<h2>Could not find place</h2>
 }
 
 export default UpdatePlace;
--- src/places/pages/usersPlaces.js
+++ src/places/pages/usersPlaces.js
@@ -5,42 +5,7 @@ import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
 const UsersPlaces = () => {
 
 
-  const DUMMY_PLACES = [
-    {
-      id: 'p1',
-      title: 'Empire State Building',
-      description: 'One of the most famous sky scrapers in the world!',
-      imageUrl: 'https://www.planetware.com/wpimages/2019/11/india-best-places-to-visit-agra.jpg',
-      address: "test adddress",
-      location: {
-            lat:3003333,
-            lng:72.333,
-      },
-    },
-    {
-      id: 'p2',
-      title: 'Empire State Building',
-      description: 'One of the most famous sky scrapers in the world!',
-      imageUrl: 'https://www.planetware.com/wpimages/2019/11/india-best-places-to-visit-agra.jpg',
-      address: "test adddress",
-      location: {
-            lat:3003333,
-            lng:72.333,
-      },
-    },
-    {
-      id: 'p3',
-      title: 'pink tree',
-      description: 'One of the most famous sky scrapers in the world!',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a2/1121098-pink-nature-wallpaper-1920x1080-lockscreen.jpg',
-      address: "test adddress",
-      location: {
-            lat:3003333,
-            lng:72.333,
-      },
-    },
-  ]
-
+  
   const userId = useParams().uid;
   const loadPlaces = DUMMY_PLACES.filter(place => place.creator ===userId)
 
