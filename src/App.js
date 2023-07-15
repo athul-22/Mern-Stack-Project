e44683a6dfd875a8bfe0f5545fb13c2ece96c30c
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-07-15 20:58:06 +0530

    1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NewPlaces from './places/pages/NewPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UsersPlaces from './places/pages/usersPlaces';
 import UpdatePlace from './places/pages/updatePlace.js';
+import Auth from './users/pages/auth';
 
 const App = () =>  {
   return (
@@ -17,6 +18,7 @@ const App = () =>  {
         <Route path="/places/new" exact> <NewPlaces/> </Route>
         <Route path ="/:userId/places" exact> <UsersPlaces/> </Route>
         <Route path = "/places/:placeId"> <UpdatePlace/> </Route>
+        <Route path = "/authenticaton"> <Auth/> </Route>
         {/* <Redirect to="/"/> */}
 
         </Switch>
--- src/users/pages/auth.js
+++ src/users/pages/auth.js
@@ -1,8 +1,20 @@
 import React from "react";
-import './auth.css'
+import './auth.css';
+import Card from "../../shared/components/UIElements/Cards/Card";
+import Button from "../../shared/FormElements/Button";
+import Input from "../../shared/FormElements/input";
+
 
 const Auth = () => {
-
+    return(
+        <Card className="authentication">
+            <hr/>
+            <form>
+                <Input element="input" id="email" type="email" label="E-MAIL" />
+                
+            </form>
+        </Card>
+    )
 }
 
 export default Auth;
